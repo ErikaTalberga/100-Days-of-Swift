d99a99042b314aa890ae5c2a785092b8caf9d181
ErikaTalberga <erika.talberga@gmail.com>
2024-02-01 13:26:50 +0200

    initial commit

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by erika.talberga on 01/02/2024.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
