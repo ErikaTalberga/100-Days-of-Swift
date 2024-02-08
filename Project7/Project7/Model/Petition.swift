e5b8ea3f98078f57726fd3eb54a2a9cc37ed90de
ErikaTalberga <erika.talberga@gmail.com>
2024-02-08 18:45:48 +0200

    project almost done

---
+++ Project7/Project7/Model/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by erika.talberga on 08/02/2024.
+//
+
+import UIKit
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Model/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by erika.talberga on 08/02/2024.
+//
+
+import UIKit
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
