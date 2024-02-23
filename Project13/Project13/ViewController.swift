92b0cdd872c07cf08302bafcbacbe04c65650e26
ErikaTalberga <erika.talberga@gmail.com>
2024-02-23 16:18:41 +0200

    setting up storyboard

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//    MARK: - Declarations
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+//    MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
 }
 
